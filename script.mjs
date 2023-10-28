6ebf5d734d2bb4bcb4df24f1cf68126b4916b422
Vincenzo <vincenzopallante@icoud.com>
2023-10-28 19:34:37 +0200

    feat: exercise

--- script.mjs
+++ script.mjs
@@ -1,3 +1,16 @@
+async function getResults() {
+  const players = ["Tina", "Jorge", "Julien"];
+
+  for (const player of players) {
+    try {
+      const result = await luckyDraw(player);
+      console.log(result);
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+}
+
 function luckyDraw(player) {
   return new Promise((resolve, reject) => {
     const win = Boolean(Math.round(Math.random()));
@@ -12,18 +25,4 @@ function luckyDraw(player) {
   });
 }
 
-luckyDraw("Joe")
-  .then((result) => {
-    console.log(result);
-    return luckyDraw("Caroline");
-  })
-  .then((result) => {
-    console.log(result);
-    return luckyDraw("Sabrina");
-  })
-  .then((result) => {
-    console.log(result);
-  })
-  .catch((err) => {
-    console.error(err);
-  });
+getResults();
